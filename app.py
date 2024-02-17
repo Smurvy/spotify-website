f77320422041d53b9c9e36e5e5d36df988244764
Smurvy <75571383+Smurvy@users.noreply.github.com>
2024-02-17 14:30:59 -0500

    moved some stuff around

--- app.py
+++ app.py
@@ -27,27 +27,14 @@ def get_db_connection(name_db):
     conn.row_factory = sqlite3.Row
     return conn
 
-
-def formatData(t,s):
-    if not isinstance(t,dict) and not isinstance(t,list):
-        print("\t"*s+str(t))
-    else:
-        for key in t:
-            print("\t"*s+str(key))
-            if not isinstance(t,list):
-                formatData(t[key],s+1)
-
-
 @app.route('/')
 async def hello():
-    conn = get_db_connection("database")
-    cur = conn.cursor()
-
     
-
-
     if requests.get("http://127.0.0.1:8000/api/is_playing").json()["is_playing"] == True:
-        
+        conn = get_db_connection("database")
+        cur = conn.cursor()
+
+        # update data on the webpage
         loop = asyncio.get_event_loop()
         future1 = loop.run_in_executor(None, requests.get, 'http://127.0.0.1:8000/api/current_artist')
         future2 = loop.run_in_executor(None, requests.get, 'http://127.0.0.1:8000/api/current_album')
@@ -59,61 +46,54 @@ async def hello():
         album_cover_url = await future4
 
         
-
+        # parse results
         artist = artist.json()["artist_name"]
         album = album.json()["album_name"]
         song = song.json()["song_name"]
         album_cover_url = album_cover_url.json()["album_cover_url"]
 
-        formatted_album_name = write_image(album_cover_url,album)
-        album_cover = convertToBinaryData(formatted_album_name)
+       
 
         # must be everything AFTER the last slash in the share link
         encode_url = "301e8VN6F6t9EmGM41boaN?si=efa5797342df48b1"
         pprint.pprint(requests.get(f"http://127.0.0.1:8000/api/get_playlist/songs/{encode_url}/songs").json())
         
        
-
-        if(cur.execute("SELECT * FROM songs WHERE song=?", (song,)).fetchone() != None):
-            string = cur.execute("""SELECT album_cover FROM songs WHERE song = ? """,(song,)).fetchall()[0][0]
-            base64_encoded_image = base64.b64encode(string).decode("utf-8")
-            review = cur.execute("SELECT review FROM songs WHERE song=?", (song,)).fetchall()
-
+        # checking if the the song already exists in the database
+        if cur.execute("SELECT * FROM songs WHERE song=?", (song,)).fetchone() != None:
             # this updates the play count each time the page is reloaded and the song exists in the database
             # not ideal, but too lazy to deal with it, pretty garbage
             # TODO: implement "Recently Listend" so we can check if the songn playing is new, then we only add if that song is new
-            
             cur.execute("""UPDATE songs
                         SET plays = plays + 1
                         WHERE song = ?
                         """,(song,))
 
-            #
-            
-
+            review = cur.execute("SELECT review FROM songs WHERE song=?", (song,)).fetchall()
             plays = cur.execute("""SELECT plays FROM songs WHERE song = ?""",(song,)).fetchone()[0]
+            album_blob = cur.execute("""SELECT album_cover FROM songs WHERE song = ? """,(song,)).fetchall()[0][0]
+            base64_encoded_image = base64.b64encode(album_blob).decode("utf-8")
 
 
             conn.commit()
             conn.close()
             return render_template("index.html",artist=artist,album=album,song=song,album_cover=base64_encoded_image,review=review[0][0],plays=plays)
         
-        
+        image_file = write_image(album_cover_url,album)
+        album_cover = convertToBinaryData(image_file)
 
+        # writing data
         cur.execute("""INSERT INTO songs(album,artist,song,album_cover,review,plays)
                         SELECT ?,?,?,?,"There is currently no review",1
                         WHERE NOT EXISTS(SELECT 1 FROM songs WHERE song = ?);
                         """, (album, artist, song, album_cover,song))
 
-        plays = cur.execute("""SELECT plays FROM songs WHERE song = ?""",(song,)).fetchone()[0]
-
-        # make a call to a db to get the review for the db, if there is none, return a string "No review written yet"
-        
-
+        # retrieving data from database
         review = cur.execute("SELECT review FROM songs WHERE song=?", (song,)).fetchall()
-
-        string = cur.execute("""SELECT album_cover FROM songs WHERE song = ? """,(song,)).fetchall()[0][0]
-        base64_encoded_image = base64.b64encode(string).decode("utf-8")
+        plays = cur.execute("""SELECT plays FROM songs WHERE song = ?""",(song,)).fetchone()[0]
+        # have to decode the blob data (of the album cover) in the database
+        album_blob = cur.execute("""SELECT album_cover FROM songs WHERE song = ? """,(song,)).fetchall()[0][0]
+        base64_encoded_image = base64.b64encode(album_blob).decode("utf-8")
 
 
 
@@ -122,8 +102,7 @@ async def hello():
 
         # need to start pulling pictures/data from database
         return render_template("index.html",artist=artist,album=album,song=song,album_cover=base64_encoded_image,review=review[0][0],plays=plays)
-    else:
-        
+    else:  
         return render_template("nothing_playing.html") 
 
 def convertToBinaryData(filename):
@@ -141,13 +120,8 @@ def review(song):
 def submit_review():
     
     response = dict(request.get_json())
-    print(response)
     song = response["SendInfo"]["song"]["name"]
     review = response["SendInfo"]["review"]
-
-
-    print(review)
-    print(song)
     
     conn = get_db_connection("database")
     cur = conn.cursor()
